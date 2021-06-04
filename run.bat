90bc522659e78476be77175ffbd7a7eac116b40c
UnderKo <hhit934@gmail.com>
2021-06-04 22:39:32 +0300

    Date time 22 39

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 
 git init
 git add .
-git commit -m "first commit"
+git commit -m "Date time 22 39"
 git branch -M main
 git remote add origin https://github.com/Under4groos/ImageConverter.git
 git push -u origin main
